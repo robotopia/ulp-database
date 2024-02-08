6d0ec07b7b3f1e583917e8e1f6f30d0f55fc9983
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-08 18:17:57 +0800

    Added more models, admin, and a few migrations

--- ulp/published/admin.py
+++ ulp/published/admin.py
@@ -3,4 +3,19 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(ulp)
+@admin.register(Ulp)
+class UlpAdmin(admin.ModelAdmin):
+    list_display = ['name', 'abbr']
+
+@admin.register(Article)
+class UlpAdmin(admin.ModelAdmin):
+    list_display = ['citet_text', 'doi']
+
+@admin.register(Parameter)
+class UlpAdmin(admin.ModelAdmin):
+    list_display = ['name', 'ascii_symbol', 'unit']
+
+@admin.register(Measurement)
+class UlpAdmin(admin.ModelAdmin):
+    list_display = ['ulp', 'article', 'parameter', 'formatted_quantity']
+
--- ulp/published/models.py
+++ ulp/published/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
-class ulp(models.Model):
+class Ulp(models.Model):
 
     name = models.CharField(
         max_length=63,
@@ -9,6 +10,201 @@ class ulp(models.Model):
         help_text="The official name of the ULP",
     )
 
+    abbr = models.CharField(
+        max_length=15,
+        unique=True,
+        blank=True,
+        null=True,
+        help_text="A short version of the name",
+    )
+
+    discoverer = models.ForeignKey(
+        Group,
+        null=True,
+        blank=True,
+        help_text="The group who first discovered this ULP",
+        on_delete=models.RESTRICT,
+        related_name="ulps",
+    )
+
+    readers = models.ManyToManyField(
+        User,
+        blank=True,
+        help_text="The users that have read access to this ULP",
+        related_name="readable_ulps",
+    )
+
+    editors = models.ManyToManyField(
+        User,
+        blank=True,
+        help_text="The users that have edit access to this ULP",
+        related_name="editable_ulps",
+    )
+
     class Meta:
         verbose_name = "ULP"
         verbose_name_plural = "ULPs"
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+
+    citet_text = models.CharField(
+        max_length=127,
+        blank=True,
+        null=True,
+        help_text="The display text when this is cited inline, e.g., Smith et. al (1900)",
+    )
+
+    doi = models.CharField(
+        max_length=1023,
+        blank=True,
+        null=True,
+        help_text="The DOI of the article",
+        verbose_name="DOI",
+    )
+
+    def __str__(self):
+        return self.citet_text
+
+
+class Parameter(models.Model):
+
+    name = models.CharField(
+        max_length=63,
+        unique=True,
+        help_text="The name of the parameter",
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+        help_text="A description of this measurable parameter",
+    )
+
+    ascii_symbol = models.CharField(
+        max_length=15,
+        help_text="The symbol used for this parameter, expressed in ASCII characters",
+    )
+
+    latex_symbol = models.CharField(
+        max_length=63,
+        blank=True,
+        null=True,
+        help_text="The symbol used for this parameter, expressed as a LaTeX expression",
+    )
+
+    unit = models.CharField(
+        max_length=31,
+        blank=True,
+        null=True,
+        help_text="The (astropy-conversant) unit of this parameter (e.g. 's', 'lightyear')",
+    )
+
+    prefer_scientific_notation = models.BooleanField(
+        default=False,
+        help_text="True: 3x10²; False: 300",
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.unit})"
+
+
+class Measurement(models.Model):
+
+    parameter = models.ForeignKey(
+        "Parameter",
+        help_text="The parameter beaing measured",
+        related_name="measurements",
+        on_delete=models.RESTRICT,
+    )
+
+    quantity = models.DecimalField(
+        decimal_places=20,
+        max_digits=40,
+        help_text="The numerical value of this measurement",
+    )
+
+    power_of_10 = models.IntegerField(
+        default=0,
+        help_text="The true quantity is multiplied by this power of 10. Same with the errors.",
+    )
+
+    err = models.DecimalField(
+        decimal_places=20,
+        max_digits=40,
+        null=True,
+        blank=True,
+        help_text="The (symmetrical) uncertainty on the quantity",
+    )
+
+    err_hi = models.DecimalField(
+        decimal_places=20,
+        max_digits=40,
+        null=True,
+        blank=True,
+        help_text="The (asymmetrical) upper uncertainty on the quantity",
+    )
+
+    err_lo = models.DecimalField(
+        decimal_places=20,
+        max_digits=40,
+        null=True,
+        blank=True,
+        help_text="The (asymmetrical) lower uncertainty on the quantity",
+    )
+
+    error_is_range = models.BooleanField(
+        default=False,
+        help_text="Display the error as a range, e.g. '1 - 5' instead of '3 ± 2'",
+    )
+
+    approximation = models.BooleanField(
+        default=False,
+        help_text="Display the error as a approximation, with the ~ symbol",
+    )
+
+    article = models.ForeignKey(
+        "Article",
+        help_text="The article in which this measurement is published",
+        related_name="measurements",
+        on_delete=models.RESTRICT,
+    )
+
+    ulp = models.ForeignKey(
+        "Ulp",
+        help_text="The ULP for to which this measurement applies",
+        related_name="measurements",
+        on_delete=models.RESTRICT,
+        verbose_name="ULP",
+    )
+
+    date = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="The date/time when this measurement was made",
+    )
+
+    notes = models.TextField(
+        null=True,
+        blank=True,
+        help_text="Any extra notes on this measurement",
+    )
+
+    prefer_scientific_notation = models.BooleanField(
+        default=False,
+        help_text="True: 3x10²; False: 300",
+    )
+
+    @property
+    def formatted_quantity(self):
+        retstr = f"{self.quantity}"
+        if self.power_of_10 != 0:
+            retstr += f" × 10^{self.power_of_10}"
+        retstr += f" {self.parameter.unit}"
+        return retstr
+
+    def __str__(self):
+        return self.formatted_quantity
