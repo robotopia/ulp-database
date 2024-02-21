f57d98e252b0245225605886392ce7bc2d945e44
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-21 20:01:36 +0800

    Added local allowed hosts

--- ulp-webapp/ulp-webapp/settings.py
+++ ulp-webapp/ulp-webapp/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '0.0.0.0',
+    'localhost',
+]
 
 LOGIN_REDIRECT_URL = '/published/galactic_view'
 LOGIN_URL = '/published/login'
