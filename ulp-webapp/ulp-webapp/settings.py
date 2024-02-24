79107f5f9807b71e58fc7a132c7ec0179be2e75b
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-24 12:42:28 +0800

    Fixed env var in settings.py

--- ulp-webapp/ulp-webapp/settings.py
+++ ulp-webapp/ulp-webapp/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') == True
+print('f{DEBUG = }')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
