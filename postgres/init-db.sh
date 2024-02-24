6cdb6d63fb262248452e159264e7bedd2596272f
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-24 09:29:13 +0800

    Fixed more syntax errors in postgres init script

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -4,8 +4,9 @@ set -e
 # Initialise the DB with the following 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"  <<-EOSQL
     CREATE DATABASE $DBNAME;
-    CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASS';"
-    ALTER ROLE $DBUSER SET client_encoding TO 'utf8';"
-    ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed';"
-    ALTER ROLE $DBUSER SET timezone TO 'UTC';"
+    CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASS';
+    ALTER ROLE $DBUSER SET client_encoding TO 'utf8';
+    ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed';
+    ALTER ROLE $DBUSER SET timezone TO 'UTC';
+    GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;
 EOSQL
