4847e24d8535a78bc63679f60c3e9fe137bc38f3
Sam McSweeney <sammy.mcsweeney@gmail.com>
2024-02-24 09:05:02 +0800

    Made postgres container init more conforming to https://github.com/docker-library/docs/blob/master/postgres/README.md

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -2,9 +2,10 @@
 set -e
 
 # Initialise the DB with the following 
-# Have to do this per line otherwise postgres complains
-psql -v ON_ERROR_STOP=1 -c "CREATE DATABASE $DBNAME;"
-psql -v ON_ERROR_STOP=1 -c "CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASS';"
-psql -v ON_ERROR_STOP=1 -c "ALTER ROLE $DBUSER SET client_encoding TO 'utf8';"
-psql -v ON_ERROR_STOP=1 -c "ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed';"
-psql -v ON_ERROR_STOP=1 -c "ALTER ROLE $DBUSER SET timezone TO 'UTC';"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"  <<-EOSQL
+    CREATE DATABASE $DBNAME;
+    CREATE USER $DBUSER WITH ENCRYPTED PASSWORD '$DBPASS';"
+    ALTER ROLE $DBUSER SET client_encoding TO 'utf8';"
+    ALTER ROLE $DBUSER SET default_transaction_isolation TO 'read committed';"
+    ALTER ROLE $DBUSER SET timezone TO 'UTC';"
+EOSQL
