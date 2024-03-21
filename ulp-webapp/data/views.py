4af79f1313183a861d2c51893878bdd93be0d35e
Ubuntu <ubuntu@ulp-webapp.novalocal>
2024-03-21 10:26:31 +0000

    Getting residual plot working still. WIP

--- ulp-webapp/data/views.py
+++ ulp-webapp/data/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from . import models
 
 from published import models as published_models
 from published.views import get_accessible_measurements
@@ -11,8 +13,6 @@ def timing_residual_view(request, pk):
 
     # Retrieve the selected ULP
     ulp = get_object_or_404(published_models.Ulp, pk=pk)
-    print(f'{ulp = }')
-    print(f'{request.user = }')
 
     # Make sure the user has the permissions to view this ULP
 
@@ -20,23 +20,42 @@ def timing_residual_view(request, pk):
     if not request.user.is_authenticated:
         return HttpResponse(status=404)
 
-    print('User is authenticated!')
-
     # Second, they have to belong to a group that has been granted access to
     # this ULP's data
     if not ulp.data_access_groups.filter(user=request.user).exists():
         return HttpResponse(status=404)
-    print('User has data access privileges for this ULP!')
 
     # Otherwise, grant them access, and get the TOAs!
-    print(models.TimeOfArrival.objects.all())
     toas = models.TimeOfArrival.objects.filter(ulp=ulp)
-    print("toas retrieved!")
+
+    # Get available published periods
+    periods = published_models.Measurement.objects.filter(
+        parameter__name="Period", # Hard code this specific parameter name
+        ulp=ulp,
+    )
+    periods = periods.filter(
+        Q(article__isnull=False) |  # It's published, and therefore automatically accessible by everyone
+        Q(owner=request.user) |  # The owner can always see their own measurements
+        Q(access=published_models.Measurement.ACCESS_PUBLIC) |  # Include measurements explicitly marked as public
+        (Q(access=published_models.Measurement.ACCESS_GROUP) &  # But if it's marked as group-accessible...
+         Q(access_groups__in=request.user.groups.all()))  # ...then the user must be in of the allowed groups.
+    )
+
+    # Get some bounds for the x-axis
+    mjd_min = float(toas.order_by('mjd').first().mjd)
+    mjd_max = float(toas.order_by('-mjd').first().mjd)
+    mjd_range = mjd_max - mjd_min
+    plot_specs = {
+        'xmin': mjd_min - mjd_range*0.05,
+        'xmax': mjd_max + mjd_range*0.05,
+        'xrange': mjd_range,
+    }
 
     context = {
         'ulp': ulp,
         'toas': toas,
+        'periods': periods,
+        'plot_spec': plot_specs,
     }
-    print("Context generated")
 
     return render(request, 'data/timing_residuals.html', context)
